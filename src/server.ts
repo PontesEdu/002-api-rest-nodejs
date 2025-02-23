67599bfe89c01bfb78fec1040f55e5721b07924d
Eduardo Pontes da Silva <edupontessilva03@gmail.com>
2025-02-23 12:17:44 -0300

    mudei algumas config do port

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import { app } from './app'
 import { env } from './env'
 
-app
-  .listen({
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log('Http server Running!')
-  })
+const port = env.PORT
+const host = 'RENDER' in process.env ? '0.0.0.0' : 'localhost'
+
+app.listen({ host, port }).then(() => {
+  console.log('Http server Running!')
+})
